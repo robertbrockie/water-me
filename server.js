c35ec484ad05211a4aafe6be1258a1f904887feb
Robert Brockie <robertbrockie@gmail.com>
2018-03-11 22:57:58 -0400

    Testing messages.

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var path = require('path');
+var twilio = require('twilio');
 
 require('dotenv').config()
 
@@ -8,6 +9,21 @@ var port = process.env.PORT || 3000;
 var app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/send_message', function(req, res) {
+    var client = new twilio(process.env.TWILIO_SID, process.env.TWILIO_TOKEN);
+
+    client.messages.create({
+            body: 'Hello Brock from Node',
+            to: process.env.MY_PHONE,
+            from: process.env.TWILIO_PHONE
+    }).then((message) => {
+        console.log(message.sid)
+        res.send({
+            'sid': message.sid
+        });
+    });
+});
 
 var server = app.listen(port, '0.0.0.0', function() {
 	console.log('Listening on port %d', server.address().port);
