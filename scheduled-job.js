cdb3975006f4fdcb0b5904b7ccba58c027322588
Robert Brockie <robertbrockie@gmail.com>
2018-03-17 22:12:19 -0400

    Clean up message.

--- scheduled-job.js
+++ scheduled-job.js
@@ -1,11 +1,22 @@
-var twilio = require('twilio')
+let moment = require('moment')
+let twilio = require('twilio')
 require('dotenv').config()
+
+const ALERT_DAY = 5 // Friday
+
+function generateMessage() {
+    if (moment().getDay() === ALERT_DAY) {
+        return 'Do remember to water your plants today!'
+    } else {
+        return 'It\'s only ' + moment().format('dddd') + ' no need to worry about your plants today!'
+    }
+}
 
 function sendReminderText() {
     var client = new twilio(process.env.TWILIO_SID, process.env.TWILIO_TOKEN);
 
     client.messages.create({
-            body: 'Water your plants idiot!',
+            body: generateMessage(),
             to: process.env.MY_PHONE,
             from: process.env.TWILIO_PHONE
     }).then((message) => {
